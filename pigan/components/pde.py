9e79e8c7072c905a4a29b9b0349e240d51c8e0d4
Patrick Leser <patrick.e.leser@gmail.com>
2023-06-30 09:17:35 -0400

    added scaling of u displacement derivs

--- pigan/components/pde.py
+++ pigan/components/pde.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 
 class PDE():
-    def __init__(self):
-        pass
+    def __init__(self, scale_factor=1):
+        self._scale_factor = scale_factor
+
     def evaluate_loss(self, terms, tape):
         mse = tf.keras.losses.MeanSquaredError()
         real_output = self.calculate_residual(terms,tape)
@@ -42,6 +43,19 @@ class PDE():
 
         uy_dxx, uy_dxy = self.get_partial_derivatives(uy_dx, X, tape)
         uy_dyx, uy_dyy = self.get_partial_derivatives(uy_dy, X, tape)
+
+        # HACK: rescale the displacements; safer to do individually like this?
+        ux, uy = ux * self._scale_factor, uy * self._scale_factor
+
+        ux_dx, ux_dy = ux_dx * self._scale_factor, ux_dy * self._scale_factor
+        uy_dx, uy_dy = uy_dx, uy_dy * self._scale_factor
+                                     
+        ux_dxx, ux_dxy = ux_dxx* self._scale_factor, ux_dxy * self._scale_factor
+        ux_dyx, ux_dyy = ux_dyx* self._scale_factor, ux_dyy * self._scale_factor
+
+        uy_dxx, uy_dxy = uy_dxx* self._scale_factor, uy_dxy * self._scale_factor
+        uy_dyx, uy_dyy = uy_dyx* self._scale_factor, uy_dyy * self._scale_factor
+        # END HACK
         
         dE_dx, dE_dy = self.get_partial_derivatives(E,X,tape)
         
