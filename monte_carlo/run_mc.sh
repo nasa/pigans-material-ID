d2885d8ec17892a16a8eafa960429fec7068a707
Patrick Leser <patrick.e.leser@gmail.com>
2023-07-12 12:09:34 -0400

    modified to write output to nobackup

--- monte_carlo/run_mc.sh
+++ monte_carlo/run_mc.sh
@@ -7,16 +7,16 @@ module load ABAQUS_2021
 curr_dir=`pwd`
 temp_dir=tmp_`date +"%m-%d-%y-%s"`
 sample_dir=`realpath $1`
-output_file=output_`date +"%m-%d-%y-%s.txt"`
 script=$curr_dir"/run_sample.py"
 
 mkdir $temp_dir
 cd $temp_dir
 
 for filename in `ls $sample_dir/*.csv`; do
+output_file=$filename.out
 abaqus cae noGUI=$script -- $filename >> $output_file
+mv $output_file $sample_dir
 done
 
-mv $output_file $sample_dir
 cd $curr_dir
 rm -rf $temp_dir
